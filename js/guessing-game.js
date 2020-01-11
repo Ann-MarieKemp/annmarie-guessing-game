64cbc3dc7e437c635b7a76b5f831cfe6b81630ef
Ann-Marie Kemp <aedalenb@gmail.com>
2020-01-11 00:26:20 -0500

    added comments to js file and fixed hint button end game glitch

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -9,12 +9,12 @@ a user clicks a button or adds a guess to the input field.
 */
 // alert();
 
-
+//generates the winning number for the game
 function generateWinningNumber(){
     let answerNum = Math.ceil(Math.random() * 100);
     return answerNum;
 }
-
+//shuffles function used later to shuffle the hint array
 function shuffle(array){
     let m = array.length;
 
@@ -38,13 +38,15 @@ class Game{
         this.playersGuess = null;
         this.pastGuesses = [];
         this.winningNumber = generateWinningNumber();
+        this.isPlaying = true;
     }
+    //returns the absolute value of the difference between numbers
     difference(numA, numB){
         numA = this.playersGuess;
         numB = this.winningNumber;
         return Math.abs(numA - numB);
     }
-
+    //returns which number is lower, not actually used in rest of solution
     isLower(numA, numB){
         numA = this.playersGuess;
         numB = this.winningNumber;
@@ -53,7 +55,7 @@ class Game{
         }
         return false;
     }
-
+    //returns the check guess function after checking that the number satisfies the base requirements
     playersGuessSubmission(num){
         this.playersGuess = num;
         if ( num < 1 || num > 100 || typeof num !== 'number'|| Number.isNaN(num)){
@@ -63,38 +65,51 @@ class Game{
         return this.checkGuess(num);
     }
 
+    //this function takes the guess and returns specific outcomes for each number range
+    outcomesAndAddGuess(num){
+        let outcome = '';
+        this.pastGuesses.push(num); 
+        let diffCalc = this.difference(this.winningNumber, this.playersGuess);
+        if (diffCalc < 10){
+            outcome = `You're burning up!`;
+        }
+        else if (diffCalc < 25 && diffCalc > 10){
+            outcome = `You're lukewarm.`;
+        }
+        else if (diffCalc < 50 && diffCalc > 25){
+            outcome = `You're a bit chilly.`;
+            
+        } else if (diffCalc < 100 && diffCalc > 50){
+            outcome = `You're ice cold!`;
+        } 
+        return outcome;
+    }
+//returns the appropriate outcome based on the input number
     checkGuess(num){
         this.playersGuess = num;
         let outcome = '';
-
+        //win condition:
         if (num === this.winningNumber){
             outcome =  'You Win!'
+            this.isPlaying = false; 
         }
+        //if number has already been guessed
         else if (this.pastGuesses.includes(num)){
             outcome =  'You have already guessed that number.';
         }
+        //runs the outcomesAndGuess function to return outcomes for each range
         else if (this.pastGuesses.length < 4){
-            this.pastGuesses.push(num);
-            let diffCalc = this.difference(this.winningNumber, this.playersGuess);
-
-            if (diffCalc < 10){
-                outcome = `You're burning up!`;
-            }
-            else if (diffCalc < 25 && diffCalc > 10){
-                outcome = `You're lukewarm.`;
-            }
-            else if (diffCalc < 50 && diffCalc > 25){
-                outcome = `You're a bit chilly.`;
-            } else if (diffCalc < 100 && diffCalc > 50){
-                outcome = `You're ice cold!`;
-            }
+            outcome = this.outcomesAndAddGuess(num);
         }
+        //otherwise you lose the game- sets isPlaying to false
         else {
             outcome = 'You Lose.';
+            this.isPlaying = false;
         }
+        //function returns the statement assigned to outcome;
         return outcome;
     }
-
+//this function provides a hint returning a shuffled array of three numbers that includes the winningNumber
     provideHint(){
         
             let winningNum = this.winningNumber;
@@ -105,29 +120,34 @@ class Game{
             return shuffedArray;
     }
 }
-
+//this function creates a new Game instance- triggered when Play Again is button is clicked
 function newGame(){
     return new Game;
 }
-
+//sets playGame equal to new game instance on page load.
 let playGame = newGame(); 
-console.log(playGame); 
+console.log(playGame);
 
 
+//this adds click event listener to the hint button, calls the provide hint function and changes the hint text to show hint array. 
 function getHintButton(){
     const hintButton = document.getElementById('hintButton');
     hintButton.addEventListener('click', () => {
-        let hintText = document.getElementById('hintyHint')
-        if(playGame.pastGuesses.length > 3){
-            console.log('getshere')
-       let hintArray = playGame.provideHint();
-       hintText.innerHTML = `The answer is one of these three Numbers: ${hintArray[0]}, ${hintArray[1]}, or ${hintArray[2]}`
-        } 
-        else {
-            hintText.innerHTML = `You can't have a hint yet- try another guess!`
+        if(playGame.isPlaying === true){
+            
+            let hintText = document.getElementById('hintyHint')
+            if (playGame.pastGuesses.length > 3){
+
+                let hintArray = playGame.provideHint();
+                hintText.innerHTML = `The answer is one of these three Numbers: ${hintArray[0]}, ${hintArray[1]}, or ${hintArray[2]}`
+            } 
+            else {
+                hintText.innerHTML = `You can't have a hint yet- try another guess!`
+            }
         }
     });
 }
+//calls getHintButton function
 getHintButton();
 
 let count = 0; 
@@ -147,8 +167,10 @@ function submitGuessAndClear(){
         }
     })
 }
+//calls submitGuessAndClear function on page load
 submitGuessAndClear();
 
+//adds click event listener to play again button and resets on page text for hints and prompts. 
 function newGameButton(){
     const newGameBut = document.getElementById('newGameButton');
     newGameBut.addEventListener('click', () => {
@@ -162,5 +184,6 @@ function newGameButton(){
         document.querySelector('.guessInput').value = ''; 
     });
 }
+//calls newGameButton function on page load.
 newGameButton();
 
